7bbda7927636a1976d0dc348dc7d5c44cbed506d
pclubSD <pclub.sd@gmail.com>
2026-01-06 23:17:01 +0900

    っっｓ

--- webwork.js
+++ webwork.js
@@ -13,5 +13,23 @@ document.addEventListener("click",e=>{
     }
 });
 
+function getform(){
+    const spreadurl="https://script.google.com/macros/s/AKfycbwi76-3T09eYUA5FmHpLjXr2jstyBc25RmI2n7XNb0niQSO6on-sYYX4e2lBZdTLq7k/exec"
+    const data={
+        name:document.getElementById("name").value,
+        mail:document.getElementById("mail").value,
+        message:document.getElementById("message").value
+    }
+    fetch(spreadurl,{
+        method:"POST",
+        mode:"no-cors",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(response=>console.log("Success:",response))
+    .catch((error)=>console.error("Error:",error));
+}
 
 
